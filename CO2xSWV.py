1116bc7cd97ff67051a38ea61a258e060d80e71e
michael huggins <michaelhuggins@protonmail.com>
2020-12-10 01:17:34 +0000

    changed fetch_data_from_NEON-API to use a delayed function in a loop and  ProgressBar

--- CO2xSWV.py
+++ CO2xSWV.py
@@ -3,8 +3,9 @@ import requests
 import hashlib
 import os
 from tqdm import tqdm  
-import glob
-
+from dask import delayed
+import dask
+from dask.diagnostics import ProgressBar
 
 def fetch_data_from_NEON_API(sitecodes, productcodes, daterange = 'most recent', data_path='/home/jovyan/NEON/CO2xSWV_data'):
     '''TODO: make a docstring for this, and move it to neon_utils when all done'''
@@ -29,33 +30,39 @@ def fetch_data_from_NEON_API(sitecodes, productcodes, daterange = 'most recent',
                 except AssertionError:
                     print('daterange must be a list, e.g. [\'2020-10\', \'2019-10\']')
                     return(None)
-
+            lazy = []
             for date in dates:
-                print(f'{product}-{site}-{date}') 
-                url = f'{base_url}data/{product}/{site}/{date}'
-                response = requests.get(url)
-                data = response.json()
-                files = data['data']['files']
-                os.makedirs(data_path, exist_ok=True)
-                for f in tqdm(files):
-                    if ('expanded' in f['name']) & ('_1' in f['name']) & (f['name'].endswith('.csv')):                        
-                        attempts = 0 
-                        while attempts < 4:
-                            # get the file 
-                            handle = requests.get(f['url'])
-                            #check the md5
-                            md5 = hashlib.md5(handle.content).hexdigest()
-                            if md5 == f['md5']:
-                                success = True
-                                attempts = 4
-                            else:
-                                print(f'md5 mismatch on attempt {attempts}')
-                                success = False
-                                attempts = attempts + 1
-                        # write the file
-                        if success:
-                            fname = data_path + f['name']
-                            with open(fname, 'wb') as sink:
-                                sink.write(handle.content)
-                    
+                result = dload(product, site, date, base_url, data_path)
+                lazy.append(result)
+            with ProgressBar():
+                dask.compute(*lazy)
+        
+@delayed                
+def dload(product, site, date, base_url, data_path):                     
+    url = f'{base_url}data/{product}/{site}/{date}'
+    response = requests.get(url)
+    data = response.json()
+    files = data['data']['files']
+    os.makedirs(data_path, exist_ok=True)
+    for f in files:
+        if ('expanded' in f['name']) & ('_1' in f['name']) & (f['name'].endswith('.csv')):                        
+            attempts = 0 
+            while attempts < 4:
+                # get the file 
+                handle = requests.get(f['url'])
+                #check the md5
+                md5 = hashlib.md5(handle.content).hexdigest()
+                if md5 == f['md5']:
+                    success = True
+                    attempts = 4
+                else:
+                    print(f'md5 mismatch on attempt {attempts}')
+                    success = False
+                    attempts = attempts + 1
+            # write the file
+            if success:
+                fname = data_path + f['name']
+                with open(fname, 'wb') as sink:
+                    sink.write(handle.content)
+            
                     
