ac815823039089ad46b1f4c95399261323170ca2
michael huggins <michaelhuggins@protonmail.com>
2020-12-12 00:28:07 +0000

    make_df is now a functional. returns a delayed df.  It had been moved from the ipynb to the py file

--- CO2xSWV.py
+++ CO2xSWV.py
@@ -2,7 +2,8 @@ import pandas as pd
 import requests
 import hashlib
 import os
-from tqdm import tqdm  
+import glob
+
 from dask import delayed
 import dask
 from dask.diagnostics import ProgressBar
@@ -65,5 +66,87 @@ def dload(product, site, date, base_url, data_path):
                 fname = data_path + f['name']
                 with open(fname, 'wb') as sink:
                     sink.write(handle.content)
-            
+
+# ------------------------------------------------------------------------------------
                     
+@delayed
+def make_df(hor, ver, date, site, data_path):
+    '''Reads  NEON 1 minute cvs for:
+           DP1.00094.001  (Soil CO2 concentrations)
+           DP1.00041.001  (Soil Temperature)
+           DP1.00095.001  (soil volumetric water content and salintiy)
+       drops entries with bad finalQF flags,
+       drops quality metric columns,
+       and returns a delayed dataframe of merged data
+       (use dask.compute() to make into pandas df).
+    
+    Arguments:
+    hor  -- String - horizontal sensor position (HOR in the 
+            NEON product readme files).
+    ver  -- String - vertical sensor position   (VER in the 
+            NEON product readme files).
+    date -- String - month of data desired. (yyyy-mm)
+    site -- String - NOEN site code (e.g. 'BART')
+    data_path -- String - path to data.
+    '''
+    # glob the filenames, only one gets globbed for each
+    hv_glob = hor + '.' + ver + '.' + ('[0-9]' * 3 )
+    co2 = glob.glob(f'{data_path}/*{site}*{hv_glob}*SCO2C_1_minute.{date}*.csv')[0]
+    h2o = glob.glob(f'{data_path}/*{site}*{hv_glob}*SWS_1_minute.{date}*.csv')[0]
+    t   = glob.glob(f'{data_path}/*{site}*{hv_glob}*ST_1_minute.{date}*.csv')[0]
+
+    # make CO2 df
+    co2 = pd.read_csv(co2, parse_dates=True, index_col='startDateTime')
+    # Fail and pass columns are redundent, we will use the fails
+    drops = [col for col in list(co2.columns) if 'Pass' in col]
+    co2.drop(drops, axis='columns', inplace=True)
+    # drop columns with bad quality flags
+    x = len(co2)
+    co2 = co2.loc[co2.finalQF == 0]
+    # now drop quality metric columns
+    qm = [col for col in list(co2.columns) if 'QM' in col]
+    qm = qm + [col for col in list(co2.columns) if 'QF' in col] + ['endDateTime']
+    co2.drop(qm, axis='columns', inplace=True)
+    #this following step should not be needed, but just in case
+    co2.dropna(inplace=True)
+
+    # make H2O df
+    h2o = pd.read_csv(h2o, parse_dates=True, index_col='startDateTime')
+    # Fail and pass columns are redundent, we will use the fails
+    drops = [col for col in list(h2o.columns) if 'Pass' in col]
+    h2o.drop(drops, axis='columns', inplace=True)
+    # drop columns with bad quality flags
+    x = len(h2o)
+    h2o = h2o.loc[(h2o.VSWCFinalQF == 0) & (h2o.VSICFinalQF == 0)]
+    # now drop quality metric columns
+    qm = [col for col in list(h2o.columns) if 'QM' in col]
+    qm = qm + [col for col in list(h2o.columns) if 'QF' in col] + ['endDateTime']
+    h2o.drop(qm, axis='columns', inplace=True)
+    #this following step should not be needed, but just in case
+    h2o.dropna(inplace=True)
+
+    # make soil temp df
+    soil_T = pd.read_csv(t, parse_dates=True, index_col='startDateTime')
+    # Fail and pass columns are redundent, we will use the fails
+    drops = [col for col in list(soil_T.columns) if 'Pass' in col]
+    soil_T.drop(drops, axis='columns', inplace=True)
+    # drop columns with bad quality flags
+    x = len(soil_T)
+    soil_T = soil_T.loc[soil_T.finalQF == 0]
+    # now drop quality metric columns
+    qm = [col for col in list(soil_T.columns) if 'QM' in col]
+    qm = qm + [col for col in list(soil_T.columns) if 'QF' in col] + ['endDateTime']
+    soil_T.drop(qm, axis='columns', inplace=True)
+    #this following step should not be needed, but just in case
+    soil_T.dropna(inplace=True)
+    
+    # merge co2, h2o, soil_T into one df
+    co2 = co2.merge(h2o, left_index=True, right_index=True)
+    co2 = co2.merge(soil_T, left_index=True, right_index=True)
+    
+    if len(co2) > 0:
+        co2 = co2.resample('1h').mean()
+           
+    return(co2)
+
+# ------------------------------------------------------------------------------------
